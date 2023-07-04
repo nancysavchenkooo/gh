3989c2fb3fb093c1e4bdb30fe5378893d75e07ef
nancysavchenkooo <83898268+nancysavchenkooo@users.noreply.github.com>
2023-07-05 03:42:25 +0700

    Add files via upload

--- fbc.py
+++ fbc.py
@@ -8,7 +8,6 @@ W = '\033[1;97m'
 def selenas(empas):
   url = 'https://b-api.facebook.com/method/auth.login'
   nganuid = str(uuid.uuid4())
-  ngasouid = 'OAuth 200424423651082|' + uuid.uuid4().hex
 
   print(ngasouid)
   data = {
@@ -27,7 +26,7 @@ def selenas(empas):
   headers = {
     'authority': 'b-api.facebook.com',
     'accept-language': 'en-US,en;q=0.9',
-    'authorization': ngasouid,
+    'authorization': 'OAuth 200424423651082|2a9918c6bcd75b94cefcbb5635c6ad16',
     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; ALE-L21 Build/HuaweiALE-L21; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/181.0.0.12.78;]'
     
   }
